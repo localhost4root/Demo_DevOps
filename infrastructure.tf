2cbb9f493fb2c88d6d8a6e6918b4158fbdbf39aa
localhost <localhost.root1.0@gmail.com>
2022-02-10 20:25:56 +0200

    Added gitignore

--- infrastructure.tf
+++ infrastructure.tf
@@ -1,42 +1,50 @@
 provider "aws"{
+  
   region = "eu-central-1"
 }
+
 resource "aws_instance" "infrastructure"{
-  ami=""
+
+  ami = "ami-0d527b8c289b4af7f"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ASG.id]
+}
+locals {
+  ports_in = [
+    443,
+    80,
+    9100,
+    8081,
+    8082
+  ]
+  ports_out = [
+    0
+  ]
 }
 resource "aws_security_group" "ASG"{
   name = "My Demo infrastructure"
   description = "In this way i try IAAC"
-}
-ingress{
-  from port = 80
-  to_port = 80
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-ingress{
-  from port = 9100
-  to_port = 9100
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-ingress{
-  from port = 8081
-  to_port = 8081
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-ingress{
-  from port = 8082
-  to_port = 8082
-  protocol = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-}
-egress{
-  from_port = 0
-  to_port = 0
-  protocol = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
+
+
+
+ dynamic "ingress" {
+    for_each = toset(local.ports_in)
+    content {
+      description      = "Inbound ports"
+      from_port        = ingress.value
+      to_port          = ingress.value
+      protocol         = "tcp"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
+
+  dynamic "egress" {
+    for_each = toset(local.ports_out)
+    content {
+      from_port        = egress.value
+      to_port          = egress.value
+      protocol         = "-1"
+      cidr_blocks      = ["0.0.0.0/0"]
+    }
+  }
 }
