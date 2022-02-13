61f0ddf79353650661a6c62c3f45eafc8002e4c0
localhost4root <85015077+localhost4root@users.noreply.github.com>
2022-02-13 11:25:38 +0200

    Update infrastructure.tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -9,35 +9,17 @@ resource "aws_instance" "infrastructure"{
   ami = "ami-0d527b8c289b4af7f"
   instance_type = "t2.micro"
   vpc_security_group_ids = [aws_security_group.ASG.id]
+  user_data = <<EOF
+#!/bin/sh
+curl -fsSL get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+EOF
   provisioner "local-exec" {
     command = "echo ${aws_instance.infrastructure.public_ip} >> /tmp/private_ips.txt"
   }
   associate_public_ip_address = true
   key_name         = "ssh-key"
-  user_data = <<EOF
-#!/bin/sh
-set -o errexit
-set -o nounset
-IFS=$(printf '\n\t')
-# Docker
-sudo apt remove --yes docker docker-engine docker.io containerd runc || true
-sudo apt update
-sudo apt --yes --no-install-recommends install apt-transport-https ca-certificates
-wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
-sudo apt update
-sudo apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
-sudo usermod --append --groups docker "$USER"
-sudo systemctl enable docker
-printf '\nDocker installed successfully\n\n'
-printf 'Waiting for Docker to start...\n\n'
-sleep 5
-# Docker Compose
-sudo wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
-sudo chmod +x /usr/local/bin/docker-compose
-sudo wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
-printf '\nDocker Compose installed successfully\n\n'
-EOF
+  
 }
 locals {
   ports_in = [
