16f011c99729ec910c86c9098715fbbad0854592
localhost4root <85015077+localhost4root@users.noreply.github.com>
2022-02-13 13:04:01 +0200

    Update infrastructure.tf

--- infrastructure.tf
+++ infrastructure.tf
@@ -19,6 +19,8 @@ resource "aws_instance" "infrastructure"{
 sudo apt-get update -y
 curl -fsSL https://get.docker.com -o get-docker.sh
 sudo sh get-docker.sh
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 EOF
   
  
